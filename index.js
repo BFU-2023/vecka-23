017ea3385c90ac0452481f4e0b7c047eeb61c49c
Maja Gedda <maja.gedda@zocom.se>
2023-06-05 11:31:23 +0200

    sista push

--- index.js
+++ index.js
@@ -19,3 +19,14 @@ function addToDisplay(value) {
     let display = document.querySelector(".calculator-display");
     display.value = display.value + val;
 };
+
+// summera det som står i displayen när vi trycker på '='-knappen
+let evalBtn = document.querySelector('#btn-eval');
+evalBtn.addEventListener("click", () => {
+    // få fram vad som står i displayen nu
+    let calculation = document.querySelector(".calculator-display").value;
+    // ta det vi har där och räkna ut via eval
+    let result = eval(calculation);
+    // lägg in resultatet i displayen istället
+    document.querySelector(".calculator-display").value = result;
+});
